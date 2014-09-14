2b7000be965228aada1c5814b9c05e7ca8c3aa93
L1nky <linky.yersin@gmail.com>
2014-09-14 18:37:38 +0200

    Update BlockSpeSand.class

--- src/main/java/linky/mods/thezeldamod/common/BlockSpeSand.java
+++ src/main/java/linky/mods/thezeldamod/common/BlockSpeSand.java
@@ -3,6 +3,7 @@ package linky.mods.thezeldamod.common;
 import java.util.List;
 import java.util.Random;
 
+import scala.Console;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
@@ -10,6 +11,7 @@ import net.minecraft.block.BlockSand;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
@@ -52,24 +54,13 @@ public class BlockSpeSand extends BlockSand
         {
             return this.iconArray[0];
         }
-        if(side == 1)
+        if(side == 0 || side == 1)
         {
-            if(metadata == 2)
+            if(0 <= metadata && metadata < 3)
             {
                 return this.speSand;
             }
-            else if(metadata == 5)
-            {
-                return this.speRedSand;
-            }
-        }
-        else if(side == 0)
-        {
-            if(metadata == 0)
-            {
-                return this.speSand;
-            }
-            else if(metadata == 3)
+            else if(3 <= metadata && metadata < 6)
             {
                 return this.speRedSand;
             }
@@ -95,5 +86,38 @@ public class BlockSpeSand extends BlockSand
     public int quantityDropped(Random p_149745_1_)
     {
         return 0;
+    }
+
+    @Override
+    public void onBlockDestroyedByPlayer(World world, int x, int y, int z, int metadata)
+    {
+        if(metadata % 3 == 2)
+        {
+            if(world.getBlock(x, y - 1, z) == TheZeldaMod.blockSpeSand)
+            {
+                if(world.getBlockMetadata(x, y - 1, z) - metadata == -1)
+                {
+                    world.setBlock(x, y - 1, z, TheZeldaMod.blockSpeSand, metadata, 3);
+                }
+                else if(world.getBlockMetadata(x, y - 1, z) - metadata == -2)
+                {
+                    world.setBlock(x, y - 1, z, Blocks.sand);
+                }
+            }
+        }
+        else if(metadata % 3 == 0)
+        {
+            if(world.getBlock(x, y + 1, z) == TheZeldaMod.blockSpeSand)
+            {
+                if(world.getBlockMetadata(x, y + 1, z) - metadata == 1)
+                {
+                    world.setBlock(x, y + 1, z, TheZeldaMod.blockSpeSand, metadata, 3);
+                }
+                else if(world.getBlockMetadata(x, y + 1, z) - metadata == 2)
+                {
+                    world.setBlock(x, y + 1, z, Blocks.sand);
+                }
+            }
+        }
     }
 }
